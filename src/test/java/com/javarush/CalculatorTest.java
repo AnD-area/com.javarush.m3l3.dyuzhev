f1ae80498c0758493199a47f38b826af97050d96
Anton Dyuzhev <prklphln@gmail.com>
2024-04-09 16:28:14 +0300

    Add annotations

--- src/test/java/com/javarush/CalculatorTest.java
+++ src/test/java/com/javarush/CalculatorTest.java
@@ -1,10 +1,26 @@
 package com.javarush;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    @BeforeAll
+    static void example1() {
+        System.out.println("START ALL");
+    }
+    @AfterAll
+    static void example2() {
+        System.out.println("END ALL");
+    }
+    @BeforeEach
+    void example3() {
+        System.out.println("START COUNTING");
+    }
+    @AfterEach
+    void example4() {
+        System.out.println("END COUNTING");
+    }
 
     @Test
     void add() {
@@ -23,7 +39,7 @@ class CalculatorTest {
     @Test
     void mul() {
         Calculator calc = new Calculator();
-        int result = calc.mul(3, 14);
+        int result = calc.mul(3, 4);
         assertEquals(12, result);
     }
 
